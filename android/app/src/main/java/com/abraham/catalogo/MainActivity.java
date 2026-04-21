303615149c06515f71d5f0ed847f64149f5c8413
AbrahamGuzmanInfy <guzman.abraham.infy@gmail.com>
2026-04-20 19:13:20 -0600

    Creación de APK

---
+++ android/app/src/main/java/com/abraham/catalogo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.abraham.catalogo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.abraham.catalogo',
+  appName: 'Petal & Co',
+  webDir: 'dist/catalogo/browser'
+};
+
+export default config;
